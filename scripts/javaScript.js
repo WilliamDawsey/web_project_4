6e167188510a51c84befe7aecf3d55acb43c4596
William <88216405+WilliamDawsey@users.noreply.github.com>
2021-11-18 18:55:20 -0700

    3rd iteration submit

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -11,26 +11,36 @@ const profileName = document.querySelector("#profile-name");
 const profileDescription = document.querySelector("#profile-description");
 
 /*open modal*/
-openModalButton.addEventListener("click", () => {
-  editProfileModal.classList.add("modal_open");
-});
+function openFormModal() {
+  openModalButton.addEventListener("click", () => {
+    editProfileModal.classList.add("modal_open");
+  });
+}
 
 /*submit to change content in profile name & description */
-editProfileForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-  editProfileModal.classList.remove("modal_open");
-});
-
-/*close modal*/
-editProfileCloseButton.addEventListener("click", () => {
-  editProfileModal.classList.remove("modal_open");
-});
-
-/*click outside modal to close */
-editProfileModal.addEventListener("click", (e) => {
-  if (e.target === editProfileModal) {
+function saveFormInput() {
+  editProfileForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
     editProfileModal.classList.remove("modal_open");
-  }
-});
+  });
+}
+
+/*close modal by clicking close-button or outside modal*/
+function closeFormModal() {
+  editProfileCloseButton.addEventListener("click", () => {
+    editProfileModal.classList.remove("modal_open");
+    editProfileForm.reset();
+  });
+  editProfileModal.addEventListener("click", (e) => {
+    if (e.target === editProfileModal) {
+      editProfileModal.classList.remove("modal_open");
+      editProfileForm.reset();
+    }
+  });
+}
+
+openFormModal();
+saveFormInput();
+closeFormModal();
